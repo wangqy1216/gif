dea4afa7723ab24f3d751472b0fb9742331b9660
Ubuntu <ubuntu@ip-172-31-20-248.us-east-2.compute.internal>
2020-12-07 07:37:43 +0000

    add gif

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Random from './components/Random_V1';
+import Tag from './components/Tag_V1';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Random GIF Application</h1>
+            <div className="main-container">
+                <Random />
+                <Tag />
+            </div>
+        </div>    
+    )
+};
+
+export default App;
---
+++ src/components/Tag_V1.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Tag = () => {
+    return (
+        <div>
+            <h1>Tag</h1>
+        </div>
+    )
+}
+
+export default Tag;
